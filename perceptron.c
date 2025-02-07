e726f26b7e12573c77ca4c1b9c2d05755275c1ea
usetoconfuse <toby.tischler@gmail.com>
2025-02-07 02:21:16 +0000

    Update perceptron.c

--- perceptron.c
+++ perceptron.c
@@ -11,22 +11,27 @@ int main() {
     };
 
     double w0 = 0, w1 = 0, w2 = 0;
+    int epoch = -1; // Increment from -1 as we have to loop an extra time to confirm stability
+    int stable = 0;
 
-    for (int k=0; k<32; k++) // The perceptron converges in 32 epochs
+    while (!stable) // Loop until convergence
     {
+        stable = 1;
         for (int i=0; i<6; i++)
         {
             double* pt = samples[i];
             double sum = w0 + w1 * pt[0] + w2 * pt[1];
             if (sum * pt[2] <= 0) // Adjust weights if incorrectly classified
             {
+                stable = 0;
                 w0 += pt[2];
                 w1 += pt[2] * pt[0];
                 w2 += pt[2] * pt[1];
             }
         }
+        epoch++;
     }
 
-    printf("w0: %.2f\nw1: %.2f\nw2: %.2f\n", w0, w1, w2);
+    printf("w0: %.2f\nw1: %.2f\nw2: %.2f\nStabilised in %d epochs", w0, w1, w2, epoch);
     return 0;
 }
