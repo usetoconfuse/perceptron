aded94d176c1e6b8f350fef47e8ac8e726683818
Toby Tischler <toby.tischler@gmail.com>
2025-02-22 19:56:33 +0000

    Rewritten to accept a variable volume of data with variable parameters (currently hard-coded)

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-This perceptron takes a set of data samples containing 3 double values, x1, x2, x3, and an outcome, -1 or 1.
+This perceptron takes a set of data samples containing an outcome, -1 or 1, and 3 double values, x1, x2, x3, which map to this outcome.
 It will iterate over the data set to determine the correct value weights, including a bias, to accurately categorise the entire set.
 
-We have weights w0 (bias), w1, w2, w3, which are all 0 to begin with.
-The perceptron evaluates each sample as w0 + w1\*x1 + w2\*x2 + w3*x3. The sign of the result determines the outcome it maps to.
+We have weights $w_0, w_1, w_2, w_3$ which are all 0 to begin with.
 
-The perceptron adjusts its weights using negative reinforcement; when it categorises a sample wrong, it adjusts its weights based on the correct outcome.
-It then iterates over the dataset until categorises every entry correctly.
+There are two learning algorithms:
+The perceptron evaluates each sample as $w_0 + w_1x_1 + w_2x_2 + w_3x_3$. A positive result maps to 1 and a negative
+result maps to -1 (0 maps to itself and is always considered incorrect).
+
+By default, if the sample is evaluated wrong, it will add the values of $x_1$, $x_2$ and $x_3$
+to the weights if the correct evaluation was 1 and subtract them if it was -1.
+
+If the adaline flag is set to 1, the set of weights is $W* = W + \rho(C - S)I$, 
+where $W*$ and $W$ are the new and old set of weights, $\rho$ is the learning parameter 
+(0.01 by default) and $I$ is the input vector, and $C-S$ is the difference between the correct
+output and the modelled output.
--- perceptron.c
+++ perceptron.c
@@ -1,40 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 int main() {
-    const double samples[6][4] = {
-        {1.7, 1,  4, -1},
-        {5.5, 2,  9,  1},
-        {2.2, 5,  6,  1},
-        {1.3, 4,  5,  1},
-        {1.4, 6, 0.7,-1},
-        {4.2, 1, 1.5,-1}
+
+    // Constant data array and dimensions, can be rewritten to accept input
+    const int SAMPLE_NUM = 6;
+    const int SAMPLE_SIZE = 4;
+
+    double data[6][4] = {
+        {-1, 1.7, 1,  4},
+        {1, 5.5, 2,  9},
+        {1, 2.2, 5,  6},
+        {1, 1.3, 4,  5},
+        {-1, 1.4, 6, 0.7},
+        {-1, 4.2, 1, 1.5}
     };
 
-    double w0 = 0, w1 = 0, w2 = 0, w3 = 0;
-    int epoch = -1; // Increment from -1 as we have to loop an extra time to confirm stability
+    double samples[SAMPLE_NUM][SAMPLE_SIZE];
+    memcpy(samples, data, sizeof(data));
+
+    double w[SAMPLE_SIZE];
+    int epoch = -1; // Loop from -1 as we have to loop an extra time to confirm stability
     int stable = 0;
+
+
+    // Iterate over data samples
+
+    while (!stable)
     const int sampleSize = sizeof(samples)/sizeof(*samples);
 
     while (!stable) // Loop until convergence
     {
         stable = 1;
-        for (int i=0; i<sampleSize; i++)
+        epoch++;
+        for (int i=0; i<SAMPLE_NUM; i++)
         {
-            const double* pt = samples[i];
-            const double sum = w0 + w1 * pt[0] + w2 * pt[1] + w3 * pt[2];
-            if (sum * pt[3] <= 0) // Adjust weights if incorrectly classified
+            const double* x = samples[i];
+            double sum = w[0];
+            for (int j=1; j<SAMPLE_SIZE; j++) {
+                sum += w[j] * x[j];
+            }
+            if (sum) sum = sum / fabs(sum); // Map to 1 or -1 if not 0
+
+            if (sum != x[0]) // Adjust weights if incorrectly classified
             {
                 stable = 0;
-                w0 += pt[3];
-                w1 += pt[3] * pt[0];
-                w2 += pt[3] * pt[1];
-                w3 += pt[3] * pt[2];
+
+                w[0] += x[0];
+                for (int j=1; j<SAMPLE_SIZE; j++) {
+                    w[j] += x[0] * x[j];
+                }
             }
         }
         epoch++;
     }
 
-    printf("w0: %.2f\nw1: %.2f\nw2: %.2f\nw3: %.2f\n"
-           "Stabilised in %d epochs", w0, w1, w2, w3, epoch);
+    printf("w[0]: %.2f\nw[1]: %.2f\nw[2]: %.2f\nw[3]: %.2f\n"
+           "Stabilised in %d epochs", w[0], w[1], w[2], w[3], epoch);
     return 0;
 }
