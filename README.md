08184f8a4160a9651b080e8fae89eab921cbc4c3
Toby Tischler <toby.tischler@gmail.com>
2025-02-23 00:06:43 +0000

    Added Adaline perceptron algorithm

--- README.md
+++ README.md
@@ -1,16 +1,59 @@
-This perceptron takes a set of data samples containing an outcome, -1 or 1, and 3 double values, x1, x2, x3, which map to this outcome.
-It will iterate over the data set to determine the correct value weights, including a bias, to accurately categorise the entire set.
+# Single-layer Perceptron
 
-We have weights $w_0, w_1, w_2, w_3$ which are all 0 to begin with.
+This algorithm takes a training set $T = \{x_0, x_1, \ldots, x_i\}$ of $m$ training vectors $x$ and calculates a weight vector $W$ that
+correctly classify all input vectors $I$ with respect to some binary class. ($T$ is currently hard-coded)
 
-There are two learning algorithms:
-The perceptron evaluates each sample as $w_0 + w_1x_1 + w_2x_2 + w_3x_3$. A positive result maps to 1 and a negative
-result maps to -1 (0 maps to itself and is always considered incorrect).
+Each $n$-dimensional vector $x$ contains the input vector $I$ with $n-1$ features followed by the correct
+classification of the data $C$.
 
-By default, if the sample is evaluated wrong, it will add the values of $x_1$, $x_2$ and $x_3$
-to the weights if the correct evaluation was 1 and subtract them if it was -1.
 
-If the adaline flag is set to 1, the set of weights is $W* = W + \rho(C - S)I$, 
-where $W*$ and $W$ are the new and old set of weights, $\rho$ is the learning parameter 
-(0.01 by default) and $I$ is the input vector, and $C-S$ is the difference between the correct
-output and the modelled output.
+That is, for all $x$:
+- $x_0 = 1$ (to represent bias)
+- $(x_0, x_1,\;\ldots\;,x_{n-1}) = I$
+- $x_n = C$
+
+
+Let $w_k$ be the $k$th weight in $W$ and let $I_x$ be the input vector contained in $x$.\
+As the perceptron assigns one weight for each feature (including the bias), $|W| = n-1$.
+
+The perceptron calculates the weighted sum $w_x$ of a given $x$ using the formula
+
+$w_x = w_0 + w_1x_1+ w_2x_2 +\ldots+w_{n-1}x_{n-1} = W \cdot I_x$ .
+
+Classification is defined by applying the Heaviside step function $H$ to $w_x$.\
+For the purposes of this program, $H(0) = 0$, i.e., a predicted value of $0$ is always an incorrect classification.
+
+# Behaviour
+
+The program prompts the user to select which artificial neural network to train on the data:
+a standard perceptron or an Adaline perceptron. They will then enter any relevant parameters.\
+The perceptron then generates the weight vector $W$ using the corresponding algorithm, and prints
+
+$w_x = w_0 + w_1x_1+ w_2x_2 +\ldots+w_{n-1}x_{n-1}$
+
+with the calculated weights $w_0, \ldots, w_{n-1}$ in $W$.
+
+## Standard
+
+The standard perceptron iterates over $T$ in order, classifying each $I_x$ as $H(w_x)$.
+
+If a point is incorrectly classified, i.e., $H(w_x) \neq C$, then the perceptron adjusts W
+with the formula
+
+$W* = W + C(I_x)$
+
+where $W*$ is the adjusted set of weights.
+
+The perceptron continues to iterate until it completes an iteration over $T$ where it correctly classifies every $I$.
+Upon completion, the program will output the number of epochs this took.
+
+## Adaline
+
+When selecting the Adaline perceptron, you will be prompted to input a learning parameter $\rho$ and a limit value.
+
+The Adaline perceptron iterates over $T$ in order like the standard perceptron. However, it does not classify each
+$I_x$. Instead, it adjusts the weights based on the error in each $w_x$ using the formula
+
+$W* = W + \rho(C-w_x)I_x$ .
+
+The limit value determines how many epochs the perceptron iterates for.
--- perceptron.c
+++ perceptron.c
@@ -2,28 +2,80 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
+
+
 
 int main() {
 
     // Constant data array and dimensions, can be rewritten to accept input
-    const int SAMPLE_NUM = 6;
-    const int SAMPLE_SIZE = 4;
+    #define m 6 // Number of training vectors
+    #define n 4 // Dimension of each training vector
 
-    double data[6][4] = {
-        {-1, 1.7, 1,  4},
-        {1, 5.5, 2,  9},
-        {1, 2.2, 5,  6},
-        {1, 1.3, 4,  5},
-        {-1, 1.4, 6, 0.7},
-        {-1, 4.2, 1, 1.5}
+    const double data[m][n] = {
+        {1, 1,  4,  -1},
+        {1, 2,  9,   1},
+        {1, 5,  6,   1},
+        {1, 4,  5,   1},
+        {1, 6, 0.7, -1},
+        {1, 1, 1.5, -1}
     };
 
-    double samples[SAMPLE_NUM][SAMPLE_SIZE];
-    memcpy(samples, data, sizeof(data));
+    double T[m][n];
+    memcpy(T, data, sizeof(data));
 
-    double w[SAMPLE_SIZE];
-    int epoch = -1; // Loop from -1 as we have to loop an extra time to confirm stability
+    double w[n-1];
+    long long epoch = 0;
     int stable = 0;
+
+    // Adaline variables
+    char adaline;
+    double lrnParam = 0;
+    long long limit = 0;
+
+    // Whether to use adaline weighting algorithm
+    while (1)
+    {
+        printf("Use adaline algorithm? (y/n)\n");
+        scanf(" %c", &adaline);
+        if (adaline == 'y' || adaline == 'n') break;
+        printf("Error: invalid input\n");
+    }
+
+    if (adaline == 'y')
+    {
+        // Randomise starting weights from 0 to 1 inclusive
+        srand(time(NULL));
+        for (int i=0; i<n-1; i++)
+        {
+            w[i] = (double)rand() / (double)RAND_MAX;
+        }
+
+        // Learning parameter for adaline weighting algorithm
+        while (1)
+        {
+            char lrnBuf[64];
+            printf("Input learning parameter (real number >0)\n");
+            scanf("%s", lrnBuf);
+            lrnParam = strtod(lrnBuf, NULL);
+            if (lrnParam > 0) break;
+            printf("Error: invalid input\n");
+        }
+
+        // Learning parameter for adaline weighting algorithm
+        while (1)
+        {
+            char limBuf[50];
+            printf("Input number of epochs to iterate for\n");
+            scanf("%s", limBuf);
+            limit = strtoll(limBuf, NULL, 10);
+            if (limit > 0) break;
+            printf("Error: invalid input\n");
+        }
+    } else // Default weighting algorithm chosen
+    {
+        epoch = -1; // Loop from -1 as we have to loop an extra time to confirm stability
+    }
 
 
     // Iterate over data samples
@@ -32,28 +84,51 @@ int main() {
     {
         stable = 1;
         epoch++;
-        for (int i=0; i<SAMPLE_NUM; i++)
+        for (int i=0; i<m; i++)
         {
-            const double* x = samples[i];
-            double sum = w[0];
-            for (int j=1; j<SAMPLE_SIZE; j++) {
+            const double* x = T[i]; // This sample
+
+            // Calculate weighted sum
+            double sum = 0;
+            for (int j=0; j<n-1; j++) {
                 sum += w[j] * x[j];
             }
-            if (sum) sum = sum / fabs(sum); // Map to 1 or -1 if not 0
 
-            if (sum != x[0]) // Adjust weights if incorrectly classified
+            if (adaline == 'y') // Adaline algorithm
             {
-                stable = 0;
+                if (epoch >= limit) // Exit if iteration limit reached
+                {
+                    break;
+                }
 
-                w[0] += x[0];
-                for (int j=1; j<SAMPLE_SIZE; j++) {
-                    w[j] += x[0] * x[j];
+                stable = 0;
+                const double diff = lrnParam * (x[n-1] - sum);
+
+                // Update weights
+                for (int j=0; j<n-1; j++)
+                {
+                    w[j] += diff * x[j];
+                }
+            } else // Default weighting algorithm
+            {
+                if (sum) sum = sum / fabs(sum); // Apply Heaviside step function
+                if (sum != x[n-1]) // Adjust weights if incorrectly classified
+                {
+                    stable = 0;
+                    for (int j=0; j<n-1; j++) {
+                        w[j] += x[n-1] * x[j];
+                    }
                 }
             }
         }
     }
 
-    printf("w[0]: %.2f\nw[1]: %.2f\nw[2]: %.2f\nw[3]: %.2f\n"
-           "Stabilised in %d epochs", w[0], w[1], w[2], w[3], epoch);
+    printf("w_x = %.4f", w[0]);
+    for (int i=1; i<n-1; i++)
+    {
+        const char sign = w[i] >= 0 ? '+' : '\0';
+        printf(" %c%.4f(x%d)", sign, w[i], i);
+    }
+    if (adaline == 'n') printf("\nStabilised in %lld epochs", epoch);
     return 0;
 }
